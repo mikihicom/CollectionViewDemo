04bd5e4dd107c354372dc991cb1131c3fb09d197
ono takeshi <onot2ndmix@gmail.com>
2012-10-23 21:50:02 +0900

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by onoT on 2012/10/23.
+//  Copyright (c) 2012年 onoT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewDemo/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  CollectionViewDemo
+//
+//  Created by onoT on 2012/10/23.
+//  Copyright (c) 2012年 onoT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
